c007f82ef91a9e2c8810199c7042dc0d7bde6a33
21000300 <21000300@LAPTOP-ATU8COS5>
2022-07-26 22:17:04 +0800

    Hello I am Jordon. I have done like 1 percent of the whole main class code. Thank you and have a nice day. Done on 26/7/2022

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Creating Array for Stalls
+		Stall[] stallArray = new Stall[10];
 
+	}
+	// Method to add stall. hahhaha
+	private static void addStall(Stall stallArray) {
+		
+	}
+	// Method to delete stall.
+	private static void deleteStall() {
+		
+	}
+	// Method to view an existing stall
+	private static void viewStall() {
+		
 	}
 
 }
