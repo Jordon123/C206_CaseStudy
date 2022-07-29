4176504ddb29ba416d87fb90e16d18565886aaf1
Pek-Shyan <106365967+Pek-Shyan@users.noreply.github.com>
2022-07-29 18:05:31 +0800

    Update C206_CaseStudy.java

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -204,9 +204,19 @@ public class C206_CaseStudy {
 		}	
 	}
 		// Method to view an existing stall
-	private void viewStall() {
-		viewAllStall();
+	public static String viewStall(ArrayList<Stall> stallArr, int StallID) {
+		
+		String output = "";
+		//String output = String.format("%-5s %-15s %-15s %-10s %-15s %-10s\n", "Stall ID","Stall Name","Operation Date","Operation Time","Category","Operator Name");
+		for(int i = 0; i < stallArr.size(); i++) {
+			if(stallArr.get(i).getId() == StallID) {
+				Stall s = stallArr.get(i);
+				output = String.format("%-5d %-15s %-15s %-10s %-15s %-10s\n", s.getId(),s.getName(),s.getOperationDate().toString(),s.getOperationTime(),s.getCategory(),s.getOperator());
+			}
+		}
+		return output;
 	}
+	
 	private Stall inputStall() {
 		int id = Helper.readInt("Enter Stall ID > ");
 		String name = Helper.readString("Enter Stall Name > ");
