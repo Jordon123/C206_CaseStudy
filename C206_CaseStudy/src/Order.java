f6679c6432c805e489d00a8b723b2051fea8ffc7
21036941 <21036941@DESKTOP-FDVCE26>
2022-07-29 16:54:32 +0800

    Order Constructor Added

--- C206_CaseStudy/src/Order.java
+++ C206_CaseStudy/src/Order.java
@@ -8,5 +8,33 @@ public class Order{
 	private String food;
 	private int price;
 	
+	public Order(int id, String name, String stall, String food, int price) {
+		this.id = id;
+		this.name = name;
+		this.stall = stall;
+		this.food = food;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getStall() {
+		return stall;
+	}
+
+	public String getFood() {
+		return food;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
 	
 }
